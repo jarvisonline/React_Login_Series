3f243e581477590ec0d57fd71eded857b90b6b1f
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-07-30 23:02:32 +0530

    reacr jwt access

---
+++ react_jwt acess/src/components/Users.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const Users = () => {
+  const [users, setUsers] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get("/api/users", {
+          signal: controller.signal,
+        });
+        console.log(response.data);
+        isMounted && setUsers(response);
+      } catch (err) {
+        console.error(err);
+        navigate("/login", { state: { from: loaction }, reolace: true });
+      }
+    };
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
+  return (
+    <article>
+      <h2>Users List</h2>
+      {users?.length ? (
+        <ul>
+          {users.map((user, i) => (
+            <li key={i}>{user?.username} </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users to dispaly</p>
+      )}
+    </article>
+  );
+};
+
+export default Users;
